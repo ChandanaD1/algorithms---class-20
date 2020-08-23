74f76eebc8a0fcc5f1b12088829ba704eeb00145
ChandanaD1 <69945596+ChandanaD1@users.noreply.github.com>
2020-08-22 20:46:34 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var moving, fixed;
+
+function setup() {
+  createCanvas(800,400);
+  fixed = createSprite(400, 200, 50, 50);
+  fixed.shapeColor = "blue";
+  moving = createSprite(200,200,80,50);
+  moving.shapeColor = "blue";
+}
+
+function draw() {
+  background(0);
+
+  moving.x = mouseX;
+  moving.y = mouseY; 
+
+  if(moving.x - fixed.x < moving.width/2 + fixed.width/2 
+    && fixed.x - moving.x < moving.width/2 + fixed.width/2
+    && moving.y - fixed.y < moving.height/2 + fixed.height/2 
+    && fixed.y - moving.y < moving.height/2 + fixed.height/2) 
+    {
+      moving.shapeColor = "green";
+      fixed.shapeColor = "green";
+    }
+    else 
+    {
+      moving.shapeColor = "blue"
+      fixed.shapeColor = "blue"
+    }
+   
+  drawSprites();
+}
